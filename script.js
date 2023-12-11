b34a42ae11ea4ff12dd287b8913f20ac7926e693
Deepak-Wagh-Project <133074729+Deepak-Wagh-Project@users.noreply.github.com>
2023-12-11 20:53:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const removeButton=document.getElementById('btn');
+const selectedElement=document.getElementById('colorSelect');
+
+removeButton.addEventListener('click',(e)=>{
+  e.preventDefault();
+ 
+  const val=selectedElement.value;
+  
+  for(i=0;i<selectedElement.options.length;i++){
+    if(selectedElement.options[i].text==val)
+{
+    selectedElement.options[i].remove();
+    return
+}  }
+
+})
